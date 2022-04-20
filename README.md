78ded05940a055816f5783ed9958fac75a72a5b1
Louis Knolle <knollelw@gmail.com>
2022-04-19 21:23:28 -0400

    late commit of updated functions and tests

--- README.md
+++ README.md
@@ -9,3 +9,28 @@ Describe creditCardValid()
 Test: "It will recognize when an empty string has been submitted and return 0."
 Code: creditCardValid("");
 Expected output: "0"
+
+Test: "It will double the even number indexes."
+Code: creditCardValid('4102080860435620');
+Expected output: '4204016604651220'
+
+Test: "It will add two digit doubled sums together."
+Code: if (number > 9) {
+      return Math.floor(number / 10 + (number % 10));
+Expected output: '42040760465320'
+
+Test: "It will add all numbers in the string together."
+Code: for (let i = 0; i < arrayDoubleDigitsAdded.length; i++) {
+    sum += parseInt(arrayDoubleDigitsAdded[i]);
+  }
+Expected output: sum(arrayDoubleDigitsAdded);
+
+Test:"It will determine if sum of all the digits in the cc# are divisible by 10."
+Code: if (sum % 10 === 0){return true}
+Output: true or false
+
+Test: "It will reverse array for all credit cards to target last digit before check number regardless of length."
+Code: (number.toString().split("")).reverse();
+Output: [1,2,3,4] -> [4,3,2,1]
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,50 @@ $(document).ready(function() {
   
 });
 
-function creditCardValid(number) {
-  if (number.trim().length > 0) {
-    let cardNumber = number.split('');
-    
+// function valid(number) {
+//   let splitNumber = parseInt(number.toString().split(""));
+//   let totalEvenValue = 0;
+//   let totalOddValue = 0;
+// for(let i = 0; i < splitNumber.length; i++) {
+// if(i % 2 === 0) {
+//   if(splitNumber[i] * 2 >= 10) {
+//     totalEvenValue += splitNumber[i] * 2 - 9;
+//   } else {
+// totalEvenValue += splitNumber[i] * 2;
+//   }
+// } else {
+//   totalOddValue += splitNumber[i];
+// }
+// }
+// return ((totalEvenValue = totalOddValue) %10 === 0)
+// }
+function isCCNumberValid(number) {
+  const ccArray = (number.toString().split("")).reverse();
+
+  const arrayWithDoubledEven = ccArray.map(function (digit, index) {
+    if (index % 2 === 0) {
+      return digit * 2;
+    } else {
+      return digit;
+    }
+  });
+
+  const arrayDoubleDigitsAdded = arrayWithDoubledEven.map(function (number) {
+    if (number > 9) {
+      return Math.floor(number / 10 + (number % 10));
+    } else {
+      return number;
+    }
+  });
+
+  let sum = 0;
+  for (let i = 0; i < arrayDoubleDigitsAdded.length; i++) {
+    sum += parseInt(arrayDoubleDigitsAdded[i]);
+  }
+
+  if (sum % 10 === 0) {
+    return true;
+  } else {
+    return false;
   }
 }
